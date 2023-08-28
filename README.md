3504ec7a5d4ccacd09ff25f8ccca15773d5d4b55
washedszn <henrywri2020@gmail.com>
2023-08-28 15:45:09 +0200

    bot changes

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# discord-rent-bot
-Discord bot which monitors new apartments being added to estate agents in the Netherlands
+# Discord Property Bot
+
+A Discord bot that fetches property listings from estate agents based on the specified city and notifies a specific channel with details of the property.
+
+**Currently supported Estate Agents:**
+
+ - Nederwoon
+
+## Setup
+
+1. **Clone the repository**:
+
+   ```
+   git clone [URL of your repository]
+   ```
+
+2. **Setup your environment**:
+
+   Rename `.env.example` to `.env` and replace the placeholders with your actual Discord bot token.
+
+   ```env
+   DISCORD_TOKEN=YOUR_DISCORD_BOT_TOKEN
+   ```
+
+3. **Setup the bot's configuration**:
+
+   Rename `config.json.example` to `config.json`. 
+
+   The file contains the following fields:
+   - `CHANNEL_ID`: The ID of the Discord channel where the bot will send property notifications.
+   - `CHECK_INTERVAL_SECONDS`: How often the bot will check for new properties.
+   - `CITY`: The city you want to search for properties in.
+
+   Fill in these fields as per your requirements.
+
+4. **Run the bot**:
+
+   ```
+   python bot.py
+   ```
+
+## Discord Commands
+
+- `set-channel`: Set the channel which will receive the property notifications.
+- `set-city [city name]`: Set the desired city for scraping.
+- `set-interval [interval in seconds]`: Set the interval for checking listings (Minimum is 60 seconds).
+
+## Note
+
+Ensure you have the necessary Python packages installed. You can install them using:
+
+```
+pip install -r requirements.txt
+```
--- bot.py
+++ bot.py
@@ -1,15 +1,41 @@
 import discord
 import os
+import json
 from discord import app_commands
 from discord.ext import tasks
+from scraper import nederwoon
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-CHANNEL_NAME = os.getenv('CHANNEL_NAME')
-BASE_URL = os.getenv('BASE_URL')
-CHECK_INTERVAL = int(os.getenv('CHECK_INTERVAL_SECONDS', 60))  # default to 60 if not set
+
+# Load config
+def load_config():
+    with open('config.json', 'r') as f:
+        return json.load(f)
+
+config = load_config()
+CHANNEL_ID = config['CHANNEL_ID']
+CHECK_INTERVAL = config['CHECK_INTERVAL_SECONDS']
+CITY = config['CITY']
+
+# Helper to update config
+def update_config(key, value):
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    
+    config[key] = value
+    
+    with open('config.json', 'w') as f:
+        json.dump(config, f, indent=4)
+
+    # Reload the local variables after updating the config
+    global CHANNEL_ID, CHECK_INTERVAL, CITY
+    config = load_config()
+    CHANNEL_ID = config['CHANNEL_ID']
+    CHECK_INTERVAL = config['CHECK_INTERVAL_SECONDS']
+    CITY = config['CITY']
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,17 +49,73 @@ tree = app_commands.CommandTree(client)
 async def on_ready():
     print(f'Logged in as {client.user.name}({client.user.id})')
     await tree.sync()
+    print('Tree synced')
+
     check_listings.start()
 
-@tasks.loop(seconds=CHECK_INTERVAL)  # Uses the CHECK_INTERVAL from .env (in seconds)
-async def check_listings():
-    # call web scrapers
-    print('run scrapers')
-    channel = client.get_channel(0)
-    #await channel.send('yo')
-    
-@tree.command(name='set-channel', description='Set the channel which receives new rental properties')
+@tree.command(name='set-channel', description='Set notification channel')
 async def set_channel_command(interaction):
-    await interaction.response.send_message('working')
+    channel_id = interaction.channel_id
+    update_config('CHANNEL_ID', channel_id)
+    await interaction.response.send_message(f'Set the notification channel to <#{channel_id}>')
+    start_or_restart_check_listings()
+
+@tree.command(name='set-city', description='Set the desired city for scraping')
+async def set_city_command(interaction, city: str):
+    update_config('CITY', city)
+    await interaction.response.send_message(f'Set the city to {city}')
+    start_or_restart_check_listings()
+
+@tree.command(name='set-interval', description='Set the interval for checking listings (For safety there is a minimum of 60 seconds)')
+async def set_interval_command(interaction, interval: int):
+    if interval < 60:
+        interval = 60
+        await interaction.response.send_message('For safety, the minimum interval is 60 seconds. Setting the check interval to 60 seconds.')
+    else:
+        await interaction.response.send_message(f'Set the check interval to {interval} seconds')
+    update_config('CHECK_INTERVAL_SECONDS', interval)
+    start_or_restart_check_listings()
+
+def start_or_restart_check_listings():
+    if check_listings.is_running():
+        check_listings.restart()
+    else:
+        check_listings.start()
+
+@tasks.loop(seconds=CHECK_INTERVAL)
+async def check_listings():
+    config = load_config()
+    # Ensure config values exist
+    if not (config.get('CHANNEL_ID') and config.get('CHECK_INTERVAL_SECONDS') and config.get('CITY')):
+        print("Incomplete config. Please ensure all settings are specified.")
+        check_listings.stop()
+        return
+    
+    properties = nederwoon()
+
+    for property_data in properties:
+        await send_embed(property_data)
+
+async def send_embed(property_data):
+    channel = client.get_channel(CHANNEL_ID)
+    
+    # Create a new embed object
+    embed = discord.Embed(
+        title=property_data['title'],
+        url=property_data['url'],
+        color=discord.Color.blue()
+    )
+    
+    # Set the first image as the main image
+    embed.set_image(url=property_data['images'][0])
+    
+    # Add each attribute as a field
+    embed.add_field(name="Address", value=property_data['address'], inline=True)
+    embed.add_field(name="Property Type", value=property_data['property_type'], inline=True)
+    embed.add_field(name="Build Status", value=property_data['build_status'], inline=True)
+    embed.add_field(name="Price", value=property_data['price'], inline=True)
+
+    # Send the embed to the channel
+    await channel.send(embed=embed)
 
 client.run(DISCORD_TOKEN)
